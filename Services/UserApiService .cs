bd41d65e527c2098e03adaca19a74d42b02ba161
Grigory-Mag <magankov2003@mail.ru>
2023-03-21 19:35:49 +0300

    CRUD operations for Ownership table

--- Services/UserApiService .cs
+++ Services/UserApiService .cs
@@ -546,32 +546,65 @@ public class UserApiService : UserService.UserServiceBase
 
     public override async Task<OwnershipsObject> GetOwnership(GetOrDeleteOwnershipsRequest request, ServerCallContext context)
     {
-        var ownership = await dbContext.Ownerships.FindAsync(request.Id);
-        if (ownership == null)
+        var item = await dbContext.Ownerships.FindAsync(request.Id);
+        if (item == null)
             throw new RpcException(new Status(StatusCode.NotFound, "Ownership not found"));
 
-        return await Task.FromResult((OwnershipsObject)ownership);
+        return await Task.FromResult((OwnershipsObject)item);
     }
 
-    public override Task<ListOwnerships> GetListOwnerships(Empty request, ServerCallContext context)
+    public override async Task<ListOwnerships> GetListOwnerships(Empty request, ServerCallContext context)
     {
-        return base.GetListOwnerships(request, context);
+        var listItems = new ListOwnerships();
+        var items = dbContext.Ownerships.Select(item =>
+            new OwnershipsObject((OwnershipsObject)item)
+        ).ToList();
+        listItems.Ownership.AddRange(items);
+        if (listItems.Ownership.Count == 0)
+            throw new RpcException(new Status(StatusCode.NotFound, "Ownerships not found"));
+
+        return await Task.FromResult(listItems);
     }
 
-    public override Task<OwnershipsObject> CreateOwnership(CreateOrUpdateOwnershipsRequest request, ServerCallContext context)
+    public override async Task<OwnershipsObject> CreateOwnership(CreateOrUpdateOwnershipsRequest request, ServerCallContext context)
     {
-        return base.CreateOwnership(request, context);
+        var item = (Ownership)request.Ownership;
+        await dbContext.Ownerships.AddAsync(item);
+        await dbContext.SaveChangesAsync();
+
+        return await Task.FromResult((OwnershipsObject)item);
     }
 
-    public override Task<OwnershipsObject> UpdateOwnership(CreateOrUpdateOwnershipsRequest request, ServerCallContext context)
+    public override async Task<OwnershipsObject> UpdateOwnership(CreateOrUpdateOwnershipsRequest request, ServerCallContext context)
     {
-        return base.UpdateOwnership(request, context);
+        var item = await dbContext.Ownerships.FindAsync(request.Ownership.Id);
+        if (item == null)
+            throw new RpcException(new Status(StatusCode.NotFound, "Ownership not found"));
+        item = (Ownership)request.Ownership;
+        await dbContext.SaveChangesAsync();
+
+        return await Task.FromResult(request.Ownership);
     }
 
-    public override Task<OwnershipsObject> DeleteOwnership(GetOrDeleteOwnershipsRequest request, ServerCallContext context)
+    public override async Task<OwnershipsObject> DeleteOwnership(GetOrDeleteOwnershipsRequest request, ServerCallContext context)
     {
-        return base.DeleteOwnership(request, context);
+        var item = await dbContext.Ownerships.FindAsync(request.Id);
+        if (item == null)
+            throw new RpcException(new Status(StatusCode.NotFound, "Ownership not found"));
+        dbContext.Ownerships.Remove(item);
+        await dbContext.SaveChangesAsync();
+
+        return await Task.FromResult((OwnershipsObject)item);
     }
+
+    /*
+* =*=*=*=*=*=*=*=*=*=*=*=*=*
+* CRUD OPERATIONS FOR 
+* --- OWNERSHIP TABLE ---
+* =*=*=*=*=*=*=*=*=*=*=*=*=*
+*/
+
+
 
     /*DBContext db;
     public UserApiService(DBContext db)
